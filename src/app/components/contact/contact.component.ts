ba5a954c3353f363d4b1446d7a69b25b10150ad4
Joran Schols <schols.joran@gmail.com>
2024-07-15 23:36:12 +0200

    contact works

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,32 +2,42 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { EmailService } from '../../services/email.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
   name: string = '';
   email: string = '';
   message: string = '';
+  isFormSubmitted: boolean = false;
 
   constructor(private emailService: EmailService) {}
 
   onSubmit(): void {
-    const subject = `Portfolio contact: ${this.name}`;
+    const subject = `Contact Form Submission from ${this.name}`;
     const body = `Name: ${this.name}\nEmail: ${this.email}\n\nMessage:\n${this.message}`;
 
     this.emailService.sendEmail(subject, body).subscribe(
       response => {
         console.log('Email sent successfully', response);
+        this.isFormSubmitted = true;
+        this.clearForm();
       },
       error => {
         console.error('Error sending email', error);
       }
     );
+  }
+
+  clearForm(): void {
+    this.name = '';
+    this.email = '';
+    this.message = '';
   }
 }
